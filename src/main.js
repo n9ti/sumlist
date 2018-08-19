4b984063435fd6b4a6eaa35045debdb09e23cbab
Nati Namvong <nati@flyingcomma.com>
2018-08-19 17:17:49 +0700

    add Loginrocket and Sentry

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import router from './router'
 import store from './store'
 
 import LogRocket from 'logrocket'
+
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
+
+Raven
+  .config('https://1d877c9bfe6141a8b781230051c32759@sentry.io/1264916')
+  .addPlugin(RavenVue, Vue)
+  .install()
+  .setDataCallback(function (data) {
+    data.extra.sessionURL = LogRocket.sessionURL
+    return data
+  })
+
 LogRocket.init('u44d3u/sumlist')
 
 Vue.config.productionTip = false
--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPlugin from 'logrocket-vuex'
+import LogRocket from 'logrocket'
+
+const logrocketPlugin = createPlugin(LogRocket)
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [logrocketPlugin],
   state: {
     todos: [
       {
