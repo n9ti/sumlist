6c8c8531a7cc72827ec97a8fa119864d46c8ae81
Nati Namvong <nati@flyingcomma.com>
2018-08-19 15:04:39 +0700

    before add storybook

--- src/store.js
+++ src/store.js
@@ -5,12 +5,47 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    todos: [
+      {
+        id: '00001',
+        name: 'iPad',
+        price: 20000,
+        isActive: true
+      }
+    ]
   },
   mutations: {
-
+    setTodos (state, todos) {
+      state.todos = todos
+    },
+    addTodoItem (state, newTodo) {
+      state.todos.push({
+        ...newTodo,
+        price: parseInt(newTodo.price),
+        id: Math.random(),
+        isActive: true
+      })
+    },
+    removeTodoItem (state, id) {
+      let index = state.todos.findIndex((item) => {
+        return item.id === id
+      })
+      state.todos.splice(index, 1)
+    },
+    toggleTodoItemStatus (state, id) {
+      let index = state.todos.findIndex((item) => {
+        return item.id === id
+      })
+      state.todos[index].isActive = !state.todos[index].isActive
+    }
+  },
+  getters: {
+    sumPrice (state) {
+      return state.todos.reduce((sum, todo) => {
+        return sum + todo.price
+      }, 0)
+    }
   },
   actions: {
-
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Sum: {{sumPrice}}</h1>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.name }} {{ todo.price }}
+        <button @click="removeTodoItem(todo.id)">x</button>
+      </li>
+    </ul>
+    <hr/>
+    <input v-model="newTodo.name">
+    <input v-model="newTodo.price" type="number">
+    <button @click="submitTodoItem">Add</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState, mapMutations, mapGetters } from 'vuex'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      newTodo: {
+        name: '',
+        price: 0
+      }
+    }
+  },
+  computed: {
+    ...mapState(['todos']),
+    ...mapGetters(['sumPrice'])
+  },
+  methods: {
+    ...mapMutations(['addTodoItem', 'removeTodoItem']),
+    submitTodoItem () {
+      this.addTodoItem(this.newTodo)
+      this.newTodo = {
+        name: '',
+        price: 0
+      }
+    }
   }
 }
 </script>
